aba996726a8da301724ecc23b3389fb0a56c634b
wen527555 <wen527555@gmail.com>
2023-03-17 13:32:59 +0800

    feat: Complete the booking page and fetch booking APIs

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from os import access
+
 import flask
 from flask import Flask
 from flask import request
@@ -49,115 +49,111 @@ def thankyou():
 def get_attractions():
     try:
         connection_object=connection_pooling.get_connection()
-        mycursor=connection_object.cursor(dictionary=True)
-        page=request.args.get("page",0,type=int)   
-        keyword=request.args.get("keyword",'')
-        per_page=12
-        mycursor.execute("SELECT COUNT(`id`) FROM attractions")
-        total_data=mycursor.fetchone()
-        total_data=total_data['COUNT(`id`)']
-        total_page=math.floor(total_data/12)
-        next_page=int(page +1) 
-        if keyword != None:
-            mycursor.execute("SELECT * FROM attractions WHERE category=%s or LOCATE(%s,`name`) LIMIT %s,12",[keyword,keyword,page*per_page])
-        else:
-            mycursor.execute("SELECT * FROM attractions LIMIT %s,12",[page*per_page])
-            
-        results=mycursor.fetchall()
-        if len(results)<12:
-            next_page = None
-        data_all=[]
-        for result in results:
-            new_images=[]
-            images=result["images"].split(",")
-            for image in images:
-                new_images.append(image)
+        with connection_object.cursor(dictionary=True) as mycursor:
+            page=request.args.get("page",0,type=int)   
+            keyword=request.args.get("keyword",'')
+            per_page=12
+            mycursor.execute("SELECT COUNT(`id`) FROM attractions")
+            total_data=mycursor.fetchone()
+            total_data=total_data['COUNT(`id`)']
+            total_page=math.floor(total_data/12)
+            next_page=int(page +1) 
+            if keyword != None:
+                mycursor.execute("SELECT * FROM attractions WHERE category=%s or LOCATE(%s,`name`) LIMIT %s,12",[keyword,keyword,page*per_page])
+            else:
+                mycursor.execute("SELECT * FROM attractions LIMIT %s,12",[page*per_page])
+                
+            results=mycursor.fetchall()
+            if len(results)<12:
+                next_page = None
+            data_all=[]
+            for result in results:
+                new_images=[]
+                images=result["images"].split(",")
+                for image in images:
+                    new_images.append(image)
 
-            data={
-            "id":result["id"],
-            "name":result["name"],
-            "category":result["category"],
-            "description":result["description"],
-            "address":result["address"],
-            "transport":result["transport"],
-            "mrt":result["mrt"],
-            "lat":result["lat"],
-            "lng":result["lng"],
-            "images":new_images           
-            }
-            data_all.append(data)
+                data={
+                "id":result["id"],
+                "name":result["name"],
+                "category":result["category"],
+                "description":result["description"],
+                "address":result["address"],
+                "transport":result["transport"],
+                "mrt":result["mrt"],
+                "lat":result["lat"],
+                "lng":result["lng"],
+                "images":new_images           
+                }
+                data_all.append(data)
 
-        if total_page >= page:
-            attractions={
-                    "nextPage":next_page,
-                    "data":data_all
-                                }
-            ans= jsonify(attractions)
-        else:
-            error={
-                "error":True,
-                "message":"無此頁面"
-            }
-            ans= jsonify(error)
-        return ans
+            if total_page >= page:
+                attractions={
+                        "nextPage":next_page,
+                        "data":data_all
+                                    }
+                ans= jsonify(attractions)
+            else:
+                error={
+                    "error":True,
+                    "message":"無此頁面"
+                }
+                ans= jsonify(error)
+            return ans
     except Exception as e:
-        return {"error": True, "message": str(e)}
+        return {"error": True, "message": str(e)},500
 
     finally:
-        mycursor.close()
         connection_object.close()   
 
 @app.route("/api/attraction/<attractionId>", methods=['GET'])
 def get_attraction(attractionId):
     try:
         connection_object=connection_pooling.get_connection()
-        mycursor=connection_object.cursor(dictionary=True)
+        with connection_object.cursor(dictionary=True) as mycursor:
+            mycursor.execute("SELECT * FROM attractions WHERE id=%s ",(attractionId,))
+            results=mycursor.fetchone()
+            if not results:
+                return{"error":True,"message":"景點編號不正確"}
+            new_images=[]
+            images=results["images"].split(",")
+            for image in images:
+                new_images.append(image)
 
-        mycursor.execute("SELECT * FROM attractions WHERE id=%s ",(attractionId,))
-        results=mycursor.fetchone()
-        if not results:
-            return{"error":True,"message":"景點編號不正確"}
-        new_images=[]
-        images=results["images"].split(",")
-        for image in images:
-            new_images.append(image)
-
-        data={
-            "id":results["id"],
-            "name":results["name"],
-            "category":results["category"],
-            "description":results["description"],
-            "address":results["address"],
-            "transport":results["transport"],
-            "mrt":results["mrt"],
-            "lat":results["lat"],
-            "lng":results["lng"],
-            "images":new_images
-        }
-        #回傳JSON格式的景點資料
-        return jsonify({"data":data})
+            data={
+                "id":results["id"],
+                "name":results["name"],
+                "category":results["category"],
+                "description":results["description"],
+                "address":results["address"],
+                "transport":results["transport"],
+                "mrt":results["mrt"],
+                "lat":results["lat"],
+                "lng":results["lng"],
+                "images":new_images
+            }
+            #回傳JSON格式的景點資料
+            return jsonify({"data":data})
 
     except Exception as e:
         return {"error": True, "message": str(e)}
 
     finally:
-        mycursor.close()
         connection_object.close()
 
 @app.route("/api/categories", methods=['GET'])
 def get_categories():
     try:
         connection_object=connection_pooling.get_connection()
-        mycursor=connection_object.cursor(dictionary=True)
-        mycursor.execute("SELECT DISTINCT category FROM attractions")
-        results=mycursor.fetchall()
-        categories=[result['category'] for result in results ]
-        return {"data":categories}
+        with connection_object.cursor(dictionary=True) as mycursor:
+            mycursor.execute("SELECT DISTINCT category FROM attractions")
+            results=mycursor.fetchall()
+            categories=[result['category'] for result in results ]
+            return {"data":categories}
     except Exception as e:
-        return {"error": True, "message": str(e)}
+        return {"error": True, "message": str(e)},500
 
     finally:
-        mycursor.close()
         connection_object.close()
 
 
@@ -167,98 +163,145 @@ def get_categories():
 def register():
     try:
         #檢查使用者名稱是否存在
-        data=request.get_json()
+        data=request.json
         print(data)
         name=data["name"]
         password=data["password"]
         email=data["email"]
         connection_object=connection_pooling.get_connection()
-        mycursor=connection_object.cursor(dictionary=True)
-        mycursor.execute("SELECT email FROM user WHERE email=%s",[email,])
-        user=mycursor.fetchone()
-        if user is None:
-            hashed_password = generate_password_hash(password, method='sha256')
-            mycursor.execute("INSERT INTO user (name,email,password) VALUES( %s, %s, %s)",[name,email,hashed_password])
-            connection_object.commit()  
-            print("註冊成功")          
-            return jsonify({"ok":True}),200
+        with connection_object.cursor(dictionary=True) as mycursor:
+            if not email or not password or not name:
+                return {"error":True,"message":"所有欄位皆須填寫，請勿空白"}
+            mycursor.execute("SELECT email FROM user WHERE email=%s",[email,])
+            user=mycursor.fetchone()
+            if not user :
+                hashed_password = generate_password_hash(password, method='sha256')
+                mycursor.execute("INSERT INTO user (name,email,password) VALUES( %s, %s, %s)",[name,email,hashed_password])
+                connection_object.commit()
+                user=mycursor.fetchone()
+                response= jsonify({"OK":True,"data":user,"message":"註冊成功！"}) 
+                return response
+            else:
+                return{"error":True,"message":"此email已註冊過，請登入"},401
 
-        #使用者名稱不存在，新增使用者
-        elif not name or not password or not email:
-            return{"error":True,"message":"所有欄位皆須填寫，請勿空白"}
-        else:
-            response={
-                "error":True,
-                "message":"此email已註冊過，請登入"
-            }
-            print("註冊失敗，此email已註冊過")   
-            return jsonify(response),400
             
 
     except Exception as e:
-        return {"error": True, "message": str(e)}
+        return {"error": True, "message": str(e)},500
 
     finally:
-        mycursor.close()
         connection_object.close()
 
-
+#會員登入狀態檢查
 @app.route('/api/user/auth',methods=['GET'])
 @jwt_required()
 def auth_get():
     try:
         connection_object=connection_pooling.get_connection()
-        mycursor=connection_object.cursor(dictionary=True)
-        email=get_jwt_identity()
-        mycursor.execute("SELECT id,name,email FROM user WHERE email=%s",[email])
-        user=mycursor.fetchone()
-        response= jsonify({"data":user}) 
-        return response
-        
+        with connection_object.cursor(dictionary=True) as mycursor:
+            email=get_jwt_identity()
+            if email:
+                mycursor.execute("SELECT id,name,email FROM user WHERE email=%s",[email])
+                user=mycursor.fetchone()
+                response= jsonify({"data":user}) 
+                return response
+            else:
+                return jsonify({"error":True,"message":"尚未登入"}),422  
 
     except Exception as e:
             return {"error": True, "message": str(e)},500
 
-
     finally:
-        mycursor.close()
         connection_object.close()
+
 
 @app.route('/api/user/auth',methods=['PUT','DELETE'])
 
 def auth():
     try:
         connection_object=connection_pooling.get_connection()
-        mycursor=connection_object.cursor(dictionary=True) 
+        with connection_object.cursor(dictionary=True) as mycursor:
+            # 會員登入
+            if request.method=='PUT':
+                data=request.get_json()
+                print(data)
+                password=data["password"]
+                email=data["email"]
+                if not email or not password:
+                    return {"error":True,"message":"請輸入完整的email及密碼"}
+                mycursor.execute("SELECT * FROM user WHERE email=%s",[email])
+                results=mycursor.fetchone()
+                if not results:
+                    return {"error":True,"message":"無此使用者，請先註冊"}
+                elif not check_password_hash(results["password"],password):
+                    return {"error":True,"message":"密碼錯誤"}
+                elif results:
 
-        # 會員登入
-        if request.method=='PUT':
-            data=request.get_json()
-            print(data)
-            password=data["password"]
-            email=data["email"]
-            if not email or not password:
-                return {"error":True,"message":"請輸入完整的email及密碼"}
-            mycursor.execute("SELECT * FROM user WHERE email=%s",[email])
-            results=mycursor.fetchone()
-            if not results:
-                return {"error":True,"message":"無此使用者，請先註冊"}
-            elif not check_password_hash(results["password"],password):
-                return {"error":True,"message":"密碼錯誤"}
-            elif results:
+                    access_token=create_access_token(identity=email)
+                    response=make_response({"ok":True})
+                    print(response)
+                    response.set_cookie("access_token", access_token, secure=False, domain='172.20.10.12',path='/')
+                    return response
 
-                access_token=create_access_token(identity=email)
+
+            # 會員登出
+            elif request.method=='DELETE':
                 response=make_response({"ok":True})
-                print(response)
-                response.set_cookie("access_token", access_token,  secure=False, domain='172.20.10.12',path='/')
+                response.delete_cookie("access_token")
                 return response
 
+    except Exception as e:
+        return {"error": True, "message": str(e)},500
 
-        # 會員登出
-        elif request.method=='DELETE':
-            response=make_response({"ok":True})
-            response.delete_cookie("access_token")
-            return response
+    finally:
+        connection_object.close()
+
+#預定行程
+
+#取得尚位確認下單的預定行程
+@app.route('/api/booking',methods=['GET'])
+@jwt_required()
+def booking_get():
+    try:
+        user_email=get_jwt_identity()
+        connection_object=connection_pooling.get_connection()
+        with connection_object.cursor(dictionary=True) as mycursor:
+            if user_email:      
+                mycursor.execute("SELECT id,name FROM user WHERE email=%s",[user_email])
+                result=mycursor.fetchone()      
+                user_id=result["id"]
+                mycursor.execute("SELECT booking.id,booking.date,booking.time,booking.price, attractions.id,attractions.name,attractions.address,attractions.images FROM booking JOIN attractions ON booking.attraction_id=attractions.id WHERE booking.user_id=%s",[user_id])
+                booking_data=mycursor.fetchone()
+                if booking_data:
+                    new_images=[]
+                    images=booking_data["images"].split(",")
+                    for image in images:
+                        new_images.append(image)         
+                    date=booking_data["date"].strftime('%Y-%-m-%-d')
+                    price=int(booking_data["price"])
+                    booking_info={
+                        "id":booking_data["id"],
+                        "username":result["name"],
+                        "date":date,
+                        "time":booking_data["time"],
+                        "price":price,
+                        "attraction":{
+                        "name":booking_data["name"],
+                        "address":booking_data["address"],
+                        "image":new_images,
+                        }                        
+                    }
+                    # print(booking_info)
+                    return jsonify({"data":booking_info}),200      
+                else:
+                    return jsonify({"data":None,"message":"沒有預定的行程"})           
+
+            else:
+                response={
+                "error":True,
+                "message":"未登入，拒絕存取"
+            } 
+                return jsonify(response),401
 
     except Exception as e:
         return {"error": True, "message": str(e)}
@@ -267,7 +310,92 @@ def auth():
         mycursor.close()
         connection_object.close()
 
+#建立新的預定行程
+@app.route('/api/booking',methods=['POST'])
+@jwt_required()
+def booking_post():
+    try:
+        user_email=get_jwt_identity()
+        data=request.json
+        date=data["date"]
+        time=data["time"]
+        price=data["price"]
+        attractionId=data["attractionId"]
+        print(attractionId,date,time,price, user_email)
+        if not date or not time or not price or not attractionId :
+            return jsonify({"error":True,"message":"所有欄位皆須填寫，請勿空白"}),400
+        connection_object=connection_pooling.get_connection()
+        with connection_object.cursor(dictionary=True) as mycursor:          
+            if user_email:      
+                mycursor.execute("SELECT id FROM user WHERE email=%s",[user_email])
+                result=mycursor.fetchone()       
+                user_id=result["id"]
+                #檢查是否有預定的行程
+                mycursor.execute("SELECT user_id FROM booking WHERE user_id=%s",[user_id])
+                pre_booking=mycursor.fetchone() 
+                if pre_booking:
+                    mycursor.execute("DELETE FROM booking WHERE user_id=%s", [pre_booking["user_id"]])
+                    connection_object.commit()                   
+                mycursor.execute("INSERT INTO booking (user_id, attraction_id, date, time, price) VALUES (%s, %s, %s, %s, %s)", [user_id, attractionId, date, time, price])
 
+                connection_object.commit()
+                data={
+                    "attractionId":attractionId,
+                    "date":date,
+                    "time":time,
+                    "price":price,
+                }
+                print(data)
+                return jsonify({"data":data})
+
+            else:
+                response={
+                "error":True,
+                "message":"未登入，拒絕存取"
+            } 
+                return jsonify(response),401
+
+    except Exception as e:
+        return {"error": True, "message": str(e)},500
+
+    finally:
+        connection_object.close()
+
+#刪除目前預定的行程
+@app.route('/api/booking',methods=['DELETE'])
+@jwt_required()
+def booking_delete():
+    try:
+        connection_object=connection_pooling.get_connection()
+        with connection_object.cursor(dictionary=True) as mycursor:
+            user_email=get_jwt_identity()
+            if user_email:
+                #找出該使用者的預定行程
+                mycursor.execute("SELECT id FROM user WHERE email=%s",[user_email])
+                result=mycursor.fetchone()       
+                user_id=result["id"]
+                #檢查是否有預定的行程
+                mycursor.execute("SELECT id FROM booking WHERE user_id=%s",[user_id])
+                booking_data=mycursor.fetchone()
+                if not booking_data:
+                    return jsonify({"error":True,"message":"沒有預定的行程"}),400
+                else:
+                    mycursor.execute("DELETE FROM booking WHERE user_id=%s",[user_id])
+                    connection_object.commit()
+                    return jsonify({"ok":True}),200
+                                        
+            else:
+                response={
+                "error":True,
+                "message":"未登入，拒絕存取"
+            } 
+                return jsonify(response),401
+
+    except Exception as e:
+        return jsonify({"error": True, "message": str(e)}),500
+
+    finally:
+        connection_object.close()
 
 
 app.run(host='0.0.0.0',port=3000,debug=True)
--- static/JS/booking.js
+++ static/JS/booking.js
@@ -1,33 +1,240 @@
 "use strict";
 
-// const btnOpenBookingPage = document.getElementById("nav_booking");
-const btnOpenBookingPage = document.querySelector(".nav_booking");
-const signInModel = document.getElementById("model_signIn");
+// import { openSignInFrom } from "./user.js";
+const btnBookingPage = document.getElementById("nav_booking");
 
-//定義取得cookie的函數;
-function getCookie(name) {
-  const cookies = document.cookie.split(";");
-  for (let i = 0; i < cookies.length; i++) {
-    const cookie = cookies[i].trim();
-    if (cookie.startsWith(name + "=")) {
-      return cookie.substring(name.length + 1);
-    }
+const order_name = document.querySelector(".order_name");
+const order_date = document.querySelector(".order_date");
+const order_time = document.querySelector(".order_time");
+const order_price = document.querySelector(".order_price");
+const order_address = document.querySelector(".order_address");
+const order_image = document.querySelector(".order_image");
+const order_username = document.querySelector(".order_username");
+const token = getCookie("access_token");
+
+function getBooking() {
+  if (!token) {
+    openSignInFrom();
+  } else {
+    fetch(`/api/booking`, {
+      method: "GET",
+      headers: {
+        Authorization: "Bearer " + token,
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          console.log("API呼叫失敗:" + response.status);
+        }
+      })
+      .then((bookingData) => {
+        renderBooking(bookingData);
+      })
+      .catch((error) => {
+        console.log("API呼叫失敗:" + error.message);
+      });
   }
-  return null;
 }
 
-const openSignInFrom = function () {
-  signInModel.classList.add("show");
-  overlay.classList.add("show");
-};
+function renderBooking(bookingData) {
+  console.log(bookingData);
+  const data = bookingData.data;
+  console.log(data);
+  const main = document.getElementById("main_booking");
+  if (data !== null) {
+    const attraction = bookingData.data.attraction;
+    const name = attraction.name;
+    const date = data.date;
+    const time = data.time;
+    const price = data.price;
+    const address = attraction.address;
+    const image = attraction.image[0];
+    const username = data.username;
+    const renderBooking = `
 
-btnOpenBookingPage.addEventListener("click", (event) => {
-  //   const token = getCookie("access_token");
-  //   if (!token) {
-  //     openSignInFrom();
-  //   } else {
-  const OpenBookingPage = event.target.closest(".nav_booking");
-  if (!OpenBookingPage) return;
-  window.location.href = `/booking`;
-  //   }
+          <div class="order_Section order_Section1">
+          <div class="order_username">您好，${username}，待預定的行程如下：</div>
+          <div class="order_image" style="background-image: url('${image}')"></div>
+          <div class="order_Section1_middle">
+            <div class="order_name">台北一日遊：${name}</div>
+            <div class="order_title2">
+              日期：
+              <div class="order_text order_date" id="order_date">${date}</div>
+            </div>
+            <div class="order_title2">
+              時間：
+              <div class="order_text order_time" id="order_time">${time}</div>
+            </div>
+            <div class="order_title2">
+              費用：
+              <div class="order_text order_price" id="order_price">新台幣${price}元</div>
+            </div>
+            <div class="order_title2">
+              地點：
+              <div class="order_text order_address" id="order_address">${address}</div>
+            </div>
+          </div>
+          <div class="order_deletebtn">
+            <img
+              src="../static/css/picture/icon_delete.png"
+              id="order_deletebtn" onclick="deleteBooking()"
+            />
+          </div>
+        </div>
+        <div class="order_Section order_Section2">
+          <hr />
+          <div class="order_title">您的聯絡資訊</div>
+          <form class="contact_form">
+            <div class="input_box">
+              <label class="order_text" for="contact_name">聯絡姓名：</label>
+              <input type="text" id="contact_name" />
+            </div>
+            <div class="input_box">
+              <label class="order_text" for="contact_email">聯絡信箱：</label>
+              <input type="email" id="contact_email" />
+            </div>
+            <div class="input_box">
+              <label class="order_text" for="contact_phone">手機號碼：</label>
+              <input type="text" id="contact_phone" />
+            </div>
+          </form>
+          <div class="order_text2">
+            請保持手機暢通，準時到達，導覽人員將用手機與您聯繫，務必留下正確的聯絡方式。
+          </div>
+        </div>
+        <div class="order_Section order_Section3">
+          <hr />
+          <div class="order_title">信用卡付款資訊</div>
+          <form class="creditCard_number">
+            <div class="input_box">
+              <label class="order_text" for="creditCard_number">卡片號碼：</label>
+              <input type="text" id="creditCard_number" />
+            </div>
+            <div class="input_box">
+              <label class="order_text" for="creditCard_date">過期時間：</label>
+              <input type="email" id="creditCard_date" />
+            </div>
+            <div class="input_box">
+              <label class="order_text" for="creditCard_password"
+                >驗證密碼：</label
+              >
+              <input type="text" id="creditCard_password" />
+            </div>
+          </form>
+        </div>
+
+        <div class="order_Section order_Section4">
+          <hr />
+          <div class="order_Section4_right">
+            <div class="order_totalPrice" id="order_totalPrice"></div>
+            <button class="payment_btn">確認訂購並付款</button>
+          </div>
+        </div>`;
+    main.insertAdjacentHTML("beforeend", renderBooking);
+
+    // const booking = ``;
+    // main.insertAdjacentHTML("beforeend", booking);
+    // order_name.textContent = "台北一日遊：" + name;
+    // order_date.textContent = date;
+    // order_time.textContent = time;
+    // order_price.textContent = "新台幣" + price + "元";
+    // order_address.textContent = address;
+    // order_image.style.backgroundImage = `url(${image})`;
+  } else {
+    const noBooking = `<div class="noBooking">目前沒有任何待預約的行程</div>`;
+    main.insertAdjacentHTML("beforeend", noBooking);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", getBooking);
+
+const nav_booking = btnBookingPage.addEventListener("click", () => {
+  if (!token) {
+    openSignInFrom();
+  } else {
+    window.location.href = `/booking`;
+  }
 });
+
+//建立旅遊行程
+function CreateBooking() {
+  const bookingDate = document.getElementById("booking_date");
+  const attraction_Id = window.location.pathname.split("/")[2];
+  let bookingPrice = null;
+  let bookingTime = null;
+  if (morning.checked) {
+    bookingTime = "早上9點至中午12點";
+    bookingPrice = 2000;
+  } else {
+    bookingTime = "下午2點至下午4點";
+    bookingPrice = 2500;
+  }
+
+  if (!token) {
+    openSignInFrom();
+  } else if (
+    bookingDate.value === "" ||
+    bookingTime === null ||
+    bookingPrice === null
+  ) {
+    console.log("所有欄位皆須填寫，請勿空白");
+  } else {
+    fetch(`/api/booking`, {
+      method: "POST",
+      body: JSON.stringify({
+        attractionId: attraction_Id,
+        date: bookingDate.value,
+        time: bookingTime,
+        price: bookingPrice,
+      }),
+      headers: {
+        Authorization: "Bearer " + token,
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        if (data !== null) {
+          window.location.href = `/booking`;
+        } else {
+          console.log("建立行程失敗");
+        }
+      })
+      .catch((error) => {
+        // console.log(error);
+        console.log("API呼叫失敗:" + error.message);
+      });
+  }
+}
+const btnCreateBooking = document.querySelector(".booking_btn");
+btnCreateBooking.addEventListener("click", CreateBooking);
+
+//刪除旅遊行程
+function deleteBooking() {
+  if (!token) {
+    openSignInFrom();
+  } else {
+    fetch(`/api/booking`, {
+      method: "DELETE",
+      headers: {
+        Authorization: "Bearer " + token,
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          console.log("API呼叫失敗:" + response.status);
+        }
+      })
+      .then((data) => {
+        location.reload();
+        console.log("已成功刪除行程");
+      });
+  }
+}
